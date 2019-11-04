51ae2ae815fd96f13db8890d2a11a04f8d13e79d
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-11-04 15:59:29 +0000

    added bookmarks and refactored the code

--- app.rb
+++ app.rb
@@ -1,20 +1,11 @@
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
-
-  get '/' do
-    'Bookmark Manager'
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :'bookmarks/index'
   end
 
-get '/bookmarks' do
-  bookmarks = [
-            "http://www.makersacademy.com",
-            "http://www.destroyallsoftware.com",
-            "http://www.google.com"
-           ]
-
-  bookmarks.join
-end
-
-run! if app_file == $0
+  run! if app_file == $0
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,12 +1,3 @@
-# first test to check if the environment is set up
-
-feature 'Viewing bookmarks' do
-    scenario 'visiting the index page' do
-      visit('/')
-      expect(page).to have_content "Bookmark Manager"
-    end
-  end
-
 
   # As a time-pressed user
   # So that I can quickly go to web sites I regularly visit
